e3750bb63f97e9339cc6b9b864876d266d4e0880
Brandon Shearrer <bshearr@outlook.com>
2023-06-03 15:52:11 -0500

    prisma and clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import { type AppType } from "next/app";
-import { api } from "y/utils/api";
-import "y/styles/globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import { type AppType } from 'next/app';
+import 'y/styles/globals.css';
+import { api } from 'y/utils/api';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<ClerkProvider {...pageProps}>
+			<Component {...pageProps} />
+		</ClerkProvider>
+	);
 };
 
 export default api.withTRPC(MyApp);
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,15 +1,18 @@
-import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "y/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from 'y/server/api/trpc';
+import { z } from 'zod';
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+	hello: publicProcedure
+		.input(z.object({ text: z.string() }))
+		.query(({ input, ctx }) => {
+			const greeting = ctx.clerk.userId
+				? `hello! ${ctx.clerk.userId}`
+				: `hello! ${input.text}`;
+			return {
+				greeting,
+			};
+		}),
+	getAll: publicProcedure.query(({ ctx }) => {
+		return ctx.prisma.locations.findMany({});
+	}),
 });
