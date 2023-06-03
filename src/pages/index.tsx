6a3a7a2c0afb8c33c43f5c7e6dc11beee5b90dde
Brandon Shearrer <bshearr@outlook.com>
2023-06-03 16:43:17 -0500

    new homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
-import { Typography } from '@mui/material';
+import {
+	Box,
+	Card,
+	CardActionArea,
+	CardContent,
+	Typography,
+} from '@mui/material';
 import { type NextPage } from 'next';
 import { api } from 'y/utils/api';
 
@@ -6,9 +12,29 @@ const Home: NextPage = () => {
 	const { data } = api.example.hello.useQuery({ text: 'from tRPC' });
 
 	return (
-		<>
-			<Typography>{data?.greeting}</Typography>
-		</>
+		<Box
+			display={'flex'}
+			justifyContent={'center'}
+			alignItems={'center'}
+			height={'100vh'}
+			width={'100vw'}
+			bgcolor={'#f5f5f5'}
+		>
+			<Card
+				sx={{
+					width: '50%',
+					height: '30%',
+					display: 'flex',
+					textAlign: 'center',
+				}}
+			>
+				<CardActionArea>
+					<CardContent>
+						<Typography>{data?.greeting}</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</Box>
 	);
 };
 
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,18 +1,22 @@
+import { clerkClient } from '@clerk/nextjs';
 import { createTRPCRouter, publicProcedure } from 'y/server/api/trpc';
 import { z } from 'zod';
 
 export const exampleRouter = createTRPCRouter({
 	hello: publicProcedure
 		.input(z.object({ text: z.string() }))
-		.query(({ input, ctx }) => {
+		.query(async ({ input, ctx }) => {
+			const clerkData = ctx.clerk.userId
+				? await clerkClient.users.getUser(ctx.clerk.userId)
+				: null;
+
 			const greeting = ctx.clerk.userId
-				? `hello! ${ctx.clerk.userId}`
+				? `hello! ${
+						clerkData?.firstName ? clerkData.firstName : input.text
+				  }`
 				: `hello! ${input.text}`;
 			return {
 				greeting,
 			};
 		}),
-	getAll: publicProcedure.query(({ ctx }) => {
-		return ctx.prisma.locations.findMany({});
-	}),
 });
