5667f027b0ca8865b7e73d85b97d43cf557bb264
Brandon Shearrer <bshearr@outlook.com>
2023-06-04 02:19:56 -0500

    hover effect to logo

--- src/components/sidenav/sidenav-items/SideNavLogo.tsx
+++ src/components/sidenav/sidenav-items/SideNavLogo.tsx
@@ -1,13 +1,23 @@
-import { ListItem } from '@mui/material';
+import { Box, ListItem } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export const SideNavLogo = () => {
 	return (
 		<ListItem sx={{ ml: -0.5 }}>
-			<Link href="/">
-				<Image src="/gitwit.svg" alt="gitwit logo" width={50} height={50} />
-			</Link>
+			<Box
+				sx={{
+					cursor: 'pointer',
+					transition: 'all 0.2s ease-in-out',
+					'&:hover': {
+						transform: 'scale(0.9)',
+					},
+				}}
+			>
+				<Link href="/">
+					<Image src="/gitwit.svg" alt="gitwit logo" width={50} height={50} />
+				</Link>
+			</Box>
 		</ListItem>
 	);
 };
