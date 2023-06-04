63820522747212e4823bf0e97f148b66c252c6ac
Brandon Shearrer <bshearr@outlook.com>
2023-06-04 01:17:46 -0500

    util comments

--- src/components/sidenav/util/sidenav-util.ts
+++ src/components/sidenav/util/sidenav-util.ts
@@ -1,7 +1,15 @@
 import theme from 'src/styles/theme';
 
+/* 
+ * Change the following values to change the color of the sidenav drawer.
+ */
 export const TEXT_COLOR = theme.palette.primary.main;
 export const HOVER_COLOR = '#f5f5f5';
+
+/*
+ * Changing the following values may result in needing to adjust margin 
+ * values in the sidenav drawer components.
+ */
 export const CLOSED_DRAWER_WIDTH = 75;
 export const OPEN_DRAWER_WIDTH = 220;
 
