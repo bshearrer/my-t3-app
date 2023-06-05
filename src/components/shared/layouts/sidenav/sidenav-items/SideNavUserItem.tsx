dd18f35ca834eed330310fb2375e73a1fea86855
Brandon Shearrer <bshearr@outlook.com>
2023-06-05 15:01:40 -0500

    update user nav bar button layouts

--- src/components/shared/layouts/sidenav/sidenav-items/SideNavUserItem.tsx
+++ src/components/shared/layouts/sidenav/sidenav-items/SideNavUserItem.tsx
@@ -7,27 +7,37 @@ type SideNavUserItemProps = {
 };
 export const SideNavUserItem = ({ textColor, open }: SideNavUserItemProps) => {
 	return (
-		<ListItem
-			sx={{
-				mt: 'auto',
-				display: 'flex',
-				alignItems: 'flex-start',
-			}}
-		>
+		<>
 			<SignedIn>
-				<Stack direction={'row'} spacing={2} alignItems={'center'}>
-					<UserButton afterSignOutUrl="/" /> {open ? <Typography noWrap>Profile</Typography> : ''}
-				</Stack>
+				<ListItem
+					sx={{
+						mt: 'auto',
+						display: 'flex',
+						alignItems: 'flex-start',
+					}}
+				>
+					<Stack direction={'row'} spacing={2} alignItems={'center'}>
+						<UserButton afterSignOutUrl="/" /> {open ? <Typography noWrap>Profile</Typography> : ''}
+					</Stack>
+				</ListItem>
 			</SignedIn>
 			<SignedOut>
-				<Box>
-					<SignInButton mode="modal">
-						<Button variant="outlined" sx={{ p: 0, color: textColor, ml: -1.4 }}>
-							Log In
-						</Button>
-					</SignInButton>
-				</Box>
+				<ListItem
+					sx={{
+						mt: 'auto',
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+				>
+					<Box>
+						<SignInButton mode="modal">
+							<Button variant="outlined" sx={{ p: 0, color: textColor, ml: -1.4 }}>
+								Log In
+							</Button>
+						</SignInButton>
+					</Box>
+				</ListItem>
 			</SignedOut>
-		</ListItem>
+		</>
 	);
 };
