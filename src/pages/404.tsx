9078e544b842fb6ff34c70078bef987ec7051ea2
Brandon Shearrer <bshearr@outlook.com>
2023-06-04 21:44:27 -0500

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,14 @@ import { Box, Typography } from '@mui/material';
 
 export default function Custom404() {
 	return (
-		<Box display={'flex'} alignItems={'center'} height={'100vh'} width={'100%'} textAlign={'center'}>
+		<Box
+			display={'flex'}
+			alignItems={'center'}
+			justifyContent={'center'}
+			height={'100vh'}
+			width={'100%'}
+			textAlign={'center'}
+		>
 			<Typography variant="h1">
 				<b style={{ color: 'red' }}>404</b> - Page Not Found 🫡
 			</Typography>
