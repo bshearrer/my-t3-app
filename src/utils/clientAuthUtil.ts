dd8ed03544b7cfdc882b2e8b397ff2c6a0cab9e2
Brandon Shearrer <bshearr@outlook.com>
2023-06-06 10:05:17 -0500

    add role type to userpublicmetadata

--- src/utils/clientAuthUtil.ts
+++ src/utils/clientAuthUtil.ts
@@ -1,5 +1,12 @@
 import { type UserResource } from '@clerk/types';
 
+declare global {
+	interface UserPublicMetadata {
+		[k: string]: unknown;
+		role?: string | string[]; // role can be a string, an array of strings, or undefined
+	}
+}
+
 export const checkUserRole = (user: UserResource | null | undefined, role: string): boolean => {
 	if (!user || !user.publicMetadata || !user.publicMetadata.role) {
 		return false;
