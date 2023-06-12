a4feb410216d2150b6a8ce3109e3843911fede9c
Brandon Shearrer <bshearr@outlook.com>
2023-06-12 15:15:12 -0500

    seth saves the day

--- src/components/forms/FormExample.tsx
+++ src/components/forms/FormExample.tsx
@@ -13,17 +13,13 @@ const validationSchema = z.object({
 	name: z.string().nonempty('Name is required'),
 	contactMethod: z.string().nonempty('Contact method is required'),
 	isSubscribed: z.string().nonempty('Subscription is required'),
-	operatingSystems: z.array(z.string()).nonempty('Operating system is required'),
+	operatingSystems: z.array(z.string()).refine((val) => val.length > 0, {
+		message: 'At least one operating system is required',
+	}),
 	date: z.date(),
 });
 
-export type FormExampleFormData = {
-	name: string;
-	contactMethod: string;
-	isSubscribed: string;
-	operatingSystems: string[];
-	date: Date;
-};
+export type FormExampleFormData = z.infer<typeof validationSchema>;
 
 const contactMethodOptions = [
 	{ value: 'email', label: 'Email' },
